a45121abc5f4ab406b89008c0d0ca5eecae20a3d
Shayon Javadizadeh <shyjavadi@gmail.com>
2014-12-14 23:18:08 -0800

    GSON and JSON

---
+++ src/main/java/WorldChat/WorldChat/InteractServlet.java
@@ -0,0 +1,31 @@
+package WorldChat.WorldChat;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+public class InteractServlet extends HttpServlet {
+   
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+    {
+    	resp.setContentType("text/plain");
+    	Gson gson = new Gson();
+    	gson.toJson(this);
+        resp.getWriter().print(gson.toJson(ChatManager.getChatHistory()));
+        
+    }
+    
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
+    {
+    	Gson gson = new Gson();
+    	String message=gson.fromJson(req.getRequestURI(), String.class);
+    	ChatManager.sendMessage(message, "API User");
+    }
+}
